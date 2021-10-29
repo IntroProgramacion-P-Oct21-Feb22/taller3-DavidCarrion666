037bbacc345e9cbf951e3decd6b559fa0c40d9a5
DavidCarrion666 <decarrion6@utpl.edu.ec>
2021-10-29 12:05:37 -0500

    primer comit

--- Ejercicio07/src/ejercicio07/Ejercicio07.java
+++ Ejercicio07/src/ejercicio07/Ejercicio07.java
@@ -29,7 +29,7 @@ public class Ejercicio07 {
         int var7 = 25;
         
         boolean resultado = ((Math.sqrt(var1) + var2) / var3) == var2 ||
-                (var4 > var5) && var6 / var7 > Math.sqrt (var4);
+                (var4 > var5) && (var6 / var7) > Math.sqrt (var6);
         
         
         System.out.println(resultado);
